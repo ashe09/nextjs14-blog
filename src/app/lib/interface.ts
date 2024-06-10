e7885a9905a405fdb86743bbe7e5c4199e9174e2
ashe09 <creation.oasis21la@gmail.com>
2024-06-10 12:28:29 +0900

    型定義のエラー判定を一部無効化

--- src/app/lib/interface.ts
+++ src/app/lib/interface.ts
@@ -2,5 +2,13 @@ export interface simpleBlogCard {
   title: string;
   smallDescription: string;
   currentSlug: string;
-  titleImage: any;
+  titleImage: unknown;
+}
+
+export interface fullBlog {
+  title: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  content: any;
+  currentSlug: string;
+  titleImage: unknown;
 }
--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -1,5 +1,5 @@
-import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
+import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: "☓☓☓☓☓☓",
@@ -9,7 +9,8 @@ export const client = createClient({
 });
 
 const builder = imageUrlBuilder(client);
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function urlFor(source: any) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   return builder.image(source);
 }
